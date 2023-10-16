815190170f389b2793511896b13d32d70366facd
Hippocampus <hippocampus.web3@proton.me>
2023-10-16 08:27:57 +0200

    Update new.js

--- new.js
+++ new.js
@@ -1,14 +1,8 @@
 const { Client: thorchainClient } = require('@xchainjs/xchain-thorchain');
 const { Network } = require('@xchainjs/xchain-client');
-const { assetToBase, assetAmount, register9Rheader } = require('@xchainjs/xchain-util');
+const { register9Rheader } = require('@xchainjs/xchain-util');
 const cosmosclient = require('@cosmos-client/core');
 const axios = require('axios');
-const { AssetRuneNative } = require('@xchainjs/xchain-util');
-const express = require('express');
-const dotenv = require('dotenv');
-
-dotenv.config();
-const phrase = process.env.PHRASE;
 
 register9Rheader(axios);
 register9Rheader(cosmosclient.default.config.globalAxios);
@@ -24,20 +18,5 @@ const thor = new thorchainClient({
   chainIds: chainIds,
 });
 
-async function depositrune(amount, memo) {
-  var DECIMAL = 8;
-  let total = assetToBase(assetAmount(Number(amount), DECIMAL));
-  try {
-    const txid = await thor.deposit({
-      "amount": total,
-      "memo": memo,
-      "asset": AssetRuneNative,
-      "walletIndex": 0
-    });
-    console.log('txid', txid)
-  } catch(e) {
-    console.log('Error', e.message);
-  }
-}
-
-depositrune(0.00001, "test");
+const address = thor.getAddress()
+console.log('address', address)
